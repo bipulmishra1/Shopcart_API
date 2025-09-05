0074f20b30d383fbcae290303ea9634e00a408c6
Bipul Mishra <mishrabipul1@gmail.com>
2025-09-05 20:11:30 +0530

    change db name

--- database.py
+++ database.py
@@ -3,6 +3,6 @@ from config import MONGO_URI
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["fastapi_auth"]
-products_collection = db["Project"]
+products_collection = db["Product"]
 users_collection = db["users"]
 orders_collection = db["orders"]
